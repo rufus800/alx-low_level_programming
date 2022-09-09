ceeea051ee8f5463e3b8938b21207bf6f1703361
rufus800 <reajoanna6@gmail.com>
2022-09-08 19:53:08 -0700

    low

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
 /**
-
-* main - Entry point
-
+* main - Ebtry point
 *
-
-* Return: Always 0 (Sucess)
-
+* Return: Always 0 (Success)
 */
-
 int main(void)
-
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
-
 }
